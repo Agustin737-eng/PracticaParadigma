49002e2b6a517a57ef655b9beb414f44d8178912
AgustinSanchez1 <agustinsanchezdev@gmail.com>
2024-11-27 21:50:12 -0300

    proyecto TPV añadido

---
+++ JavaApplication3/src/modelo/Venta.java
@@ -0,0 +1,14 @@
+
+package modelo;
+
+public class Venta {
+    int numeroFactura;
+
+    public int getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(int numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+}
